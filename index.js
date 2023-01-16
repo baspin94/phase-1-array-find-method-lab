310297786ee903bd99b05abd450623c40e33aa07
Bianca Aspin <bianca@mj94.net>
2023-01-16 14:41:09 -0800

    Completed lab.

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // code your solution here
+const records = [
+    //{ year: "2015", result: "W"},
+    { year: "2014", result: "N/A"},
+    { year: "2013", result: "L"},
+];
+
+function recordFinder(record) {
+    return (record["result"] === "W")
+};
+
+function superbowlWin(record) {
+    const tester = record.find(recordFinder);
+    if (tester !== undefined) {
+        return tester["year"];
+    } else {
+        return tester;
+    }
+};
